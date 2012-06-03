3c3aefa152b87902ccab1d49d79f6713bcf01c06
aniketpant <me@aniketpant.com>
2012-06-03 11:53:01 +0530

    added relationship
    many-to-many

--- application/models/user/details.php
+++ application/models/user/details.php
@@ -4,5 +4,9 @@ class User_Details extends Eloquent {
     
         public static $table = 'user_details';
         public static $timestamps = false;
+        
+        public function user_role() {
+                return $this->has_many_and_belongs_to('User_Role_Master', 'user_role_details');
+        }
     
 }
