7e21d4b3f7dbf4a0bd39bbe25cea8180c6fdb1c9
aniketpant <me@aniketpant.com>
2012-07-24 14:33:29 +0530

    update model
    added relationship

--- application/models/user/role/master.php
+++ application/models/user/role/master.php
@@ -5,12 +5,12 @@ class User_Role_Master extends Eloquent {
         public static $table = 'user_role_master';
         public static $timestamps = false;
         
-        public function inventory() {
+        public function inventory_type() {
                 return $this->has_many_and_belongs_to('Inventory_Type_Master', 'inventory_type_details');
         }
         
         public function details() {
-                return $this->has_many('User_Role_Details', 'user_role_details');
+                return $this->has_many_and_belongs_to('User_Details', 'user_role_details');
         }
     
 }
