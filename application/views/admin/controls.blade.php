3f116edeb55d52de2dd9bc17246b561a3dc4692e
aniketpant <me@aniketpant.com>
2012-07-24 14:34:55 +0530

    added link for new function

--- application/views/admin/controls.blade.php
+++ application/views/admin/controls.blade.php
@@ -1,18 +1,22 @@
 @layout('admin.master')
 
 @section('content')
-<h1 class="page-header">Controls</h1>
+<h1 class="page-header">Controls <small>Control panel for managing the data</small></h1>
 
-<div>
-    <ul class="unstyled">
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_user_roles') ?>" class="btn btn-large btn-primary">Manage User Roles</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_locations') ?>" class="btn btn-large btn-primary">Manage Locations</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_users') ?>" class="btn btn-large btn-primary">Manage Users</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_inventory_types') ?>" class="btn btn-large btn-primary">Manage Inventory Types</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_alloted_locations') ?>" class="btn btn-large btn-primary">Manage Alloted Location</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_alloted_inventory_types') ?>" class="btn btn-large btn-primary">Manage Alloted Inventory Types</a></li>
-        <li><a href="<?php echo url('admin/dashboard/controls/manage_alloted_inventory') ?>" class="btn btn-large btn-primary">Manage Alloted Inventory</a></li>
-    </ul>
+<div class="lead">
+    <h3>Manage Masters</h3>
+    <div class="btn-toolbar">
+        <a href="<?php echo url('admin/dashboard/controls/manage_user_roles') ?>" class="btn btn-large btn-primary">Manage User Roles</a>
+        <a href="<?php echo url('admin/dashboard/controls/manage_locations') ?>" class="btn btn-large btn-primary">Manage Locations</a>
+        <a href="<?php echo url('admin/dashboard/controls/manage_users') ?>" class="btn btn-large btn-primary">Manage Users</a>
+        <a href="<?php echo url('admin/dashboard/controls/manage_inventory_types') ?>" class="btn btn-large btn-primary">Manage Inventory Types</a>
+    </div>
+    <h3>Allotments</h3>
+    <div class="btn-toolbar">
+        <a href="<?php echo url('admin/dashboard/controls/manage_alloted_locations') ?>" class="btn btn-large btn-primary">Manage Alloted Locations</a>
+        <a href="<?php echo url('admin/dashboard/controls/manage_alloted_inventory_types') ?>" class="btn btn-large btn-primary">Manage Alloted Inventory Types</a>
+        <a href="<?php echo url('admin/dashboard/controls/manage_alloted_inventory') ?>" class="btn btn-large btn-primary">Manage Alloted Inventory</a>
+    </div>
 </div>
 
 @endsection
