35708794c28ca982616136e109fdb6af479f63dc
aniketpant <me@aniketpant.com>
2012-07-28 15:11:20 +0530

    removed the shit controller 'home'

--- application/views/home/index.blade.php
+++
@@ -1,8 +0,0 @@
-@layout('home.master')
-
-@section('content')
-<div class="hero-unit">
-    <h1>Welcome!</h1>
-    <p>Please proceed to the <a href="<?php echo url('home/login') ?>">login page</a>.</p>
-</div>
-@endsection
